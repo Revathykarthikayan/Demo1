3871c965d0e1e1bb2c892854238826c568dc5b2b
Revathykarthikayan <revathykarthikayan@gmail.com>
2024-05-12 20:23:38 +0530

    Update Demo1.py
    This is the python program for returning most frequent character of given string

--- pythonProject/new project/Demo1.py
+++ pythonProject/new project/Demo1.py
@@ -13,9 +13,7 @@ def most_freq(input_string):  # Defining a function
         return None
     else:
         return max_char  # returns the char with maximum count
-
-
-result = most_freq(input_string)
+result = most_freq(input_string)  #storeing the result of function most_freq
 if result is None:
     print("No most frequent character")
 else:
