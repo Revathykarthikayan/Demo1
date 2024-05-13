de4abb97afc799295d905558901de47fd0de1fcb
Revathykarthikayan <revathykarthikayan@gmail.com>
2024-05-13 12:54:50 +0530

    Words_strings added

---
+++ pythonProject/new project/Words_string.py
@@ -0,0 +1,3 @@
+Text=input("Enter the string to find number of words in it:")
+print("word count: ",len(Text.split()))   #using split() to separate words and len() to count words
+                                          
